0d5a2e002187fb808292a4ca962aa525df7f003e
piprock <yura.losov@gmail.com>
2026-05-15 18:53:23 +0300

    .env path fix, AddCors, test main page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { api } from './api'
 
 function App() {
+  const [genres, setGenres] = useState([])
+
+  useEffect(() => {
+    api.get('genres')
+      .then((res) => setGenres(res.data?.payload ?? []))
+      .catch(() => setGenres([]))
+  }, [])
 
   return (
     <>
-      
+      <h1>Genres</h1>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
     </>
   )
 }
